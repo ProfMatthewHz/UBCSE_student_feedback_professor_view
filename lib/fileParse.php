888a1c85d41253473630f37510bf92e945d3fde0
ProfMatthewHz <mhertz@buffalo.edu>
2021-02-25 13:56:35 -0500

    Prettied up the handling of whitespaces and moved where the manger is expected

--- lib/fileParse.php
+++ lib/fileParse.php
@@ -7,6 +7,9 @@ function parse_review_pairs($file_handle, $db_connection) {
   while (($line_text = fgetcsv($file_handle, 1000, ",")) !== FALSE) {
     $line_num = $line_num + 1;
 
+    // Clean up any whitespace oddities
+    $line_text = array_map(trim, $line_text);
+
     $line_fields = count($line_text);
 
     // Verify the current line's data seems reasonable.
@@ -14,10 +17,6 @@ function parse_review_pairs($file_handle, $db_connection) {
       $ret_val['error'] = 'Input CSV file has incorrect number of fields on line ' . $line_num;
       return $ret_val;
     } else {
-      // Clean up any whitespace oddities
-      $line_text[0] = trim($line_text[0]);
-      $line_text[1] = trim($line_text[1]);
-
       if (!email_already_exists($line_text[0], $db_connection)) {
         $ret_val['error'] = 'Input CSV file at line '. $line_num . ' includes an email that is not in system: ' . $line_text[0];
         return $ret_val;
@@ -42,16 +41,18 @@ function parse_review_teams($file_handle, $db_connection) {
   while (($line_text = fgetcsv($file_handle, 1000, ",")) !== FALSE) {
     $line_num = $line_num + 1;
 
+    // Clean up any whitespace oddities
+    $line_text = array_map(trim, $line_text);
+
     $line_fields = count($line_text);
 
     // This is a hack to avoid a trailing blank entry
-    if (empty(trim($line_text[$line_fields]))) {
+    if (empty($line_text[$line_fields])) {
       $line_fields = $line_fields - 1;
     }
 
     // Make sure the current line's data are valid
     for ($j = 0; $j < $line_fields; $j++) {
-      $line_text[$j] = trim($line_text[$j]);
       if (!email_already_exists($line_text[$j], $db_connection)) {
         $ret_val['error'] = 'Input CSV file at line '. $line_num . ' includes an email that is not in system: ' . $line_text[$j];
         return $ret_val;
@@ -78,41 +79,45 @@ function parse_review_managed_teams($file_handle, $db_connection) {
 
   $line_num = 0;
   while (($line_text = fgetcsv($file_handle, 1000, ",")) !== FALSE) {
+    $team_members = array();
+    $team_size = 0;
+
     $line_num = $line_num + 1;
+    // Clean up white space oddities
+    $line_text = array_map(trim, $line_text);
 
-    $line_fields = count($line_text);
+    $line_fields = count($line_text) - 1;
 
-    $manager = trim($line_text[0]);
+    // Make sure the current line's data are valid
+    for ($j = 0; $j < $line_fields; $j++) {
+      if (!empty($line_text[$j])) {
+        if (!email_already_exists($line_text[$j], $db_connection)) {
+          $ret_val['error'] = 'Input CSV file at line '. $line_num . ' includes an email that is not in system: ' . $line_text[$j];
+          return $ret_val;
+        } else {
+          $team_members[] = $line_text[$j];
+          $team_size = $team_size + 1;
+        }
+      }
+    }
+
+    $manager = $line_text[$line_fields];
     if (!email_already_exists($manager, $db_connection)) {
       $ret_val['error'] = 'Input CSV file at line '. $line_num . ' includes an email that is not in system: ' . $manager;
       return $ret_val;
     }
 
-    // This is a hack to avoid a trailing blank entry
-    if (empty(trim($line_text[$line_fields]))) {
-      $line_fields = $line_fields - 1;
-    }
-
-    // Make sure the current line's data are valid
-    for ($j = 1; $j < $line_fields; $j++) {
-      $line_text[$j] = trim($line_text[$j]);
-      if (!email_already_exists($line_text[$j], $db_connection)) {
-        $ret_val['error'] = 'Input CSV file at line '. $line_num . ' includes an email that is not in system: ' . $line_text[$j];
-        return $ret_val;
-      }
-    }
-
     // Now that we know data are valid, create & add all possible team pairings
-    for ($j = 1; $j < $line_fields; $j++) {
+    for ($j = 0; $j < $team_size; $j++) {
       $managed = array();
       $managed[0] = $manager;
-      $managed[1] = $line_text[$j];
+      $managed[1] = $team_members[$j];
       $ret_val[] = $managed;
 
-      for ($k = 1; $k < $line_fields; $k++) {
+      for ($k = 0; $k < $team_size; $k++) {
         $pairing = array();
-        $pairing[0] = $line_text[$j];
-        $pairing[1] = $line_text[$k];
+        $pairing[0] = $team_members[$j];
+        $pairing[1] = $team_members[$k];
         $ret_val[] = $pairing;
       }
     }
@@ -130,16 +135,15 @@ function parse_roster_file($file_handle) {
 
     $line_num = $line_num + 1;
 
+    // Clean up whitespace oddities
+    $line_text = array_map(trim, $line_text);
+
     $line_fields = count($line_text);
 
     if ($line_fields != 2) {
       $ret_val['error'] = 'Input CSV file has incorrect format at line ' . $line_num;
       return $split;
     }
-
-    // Clean up any whitespace oddities
-    $line_text[0] = trim($line_text[0]);
-    $line_text[1] = trim($line_text[1]);
 
     if (!ctype_print($line_text[0])) {
       $ret_val['error'] = 'Input CSV file includes a name ('.$line_text[0].') with unprintable characters on line ' . $line_num;
