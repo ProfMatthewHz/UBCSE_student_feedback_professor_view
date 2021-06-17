0d2c2a8db031b23e8a947c7d2c7a1ed9d199ef59
ProfMatthewHz <mhertz@buffalo.edu>
2021-06-16 21:30:28 -0400

    Removed some unneeded comments and made it easier to read.

--- peerEvalForm.php
+++ peerEvalForm.php
@@ -123,8 +123,6 @@ if ( !empty($_POST) && isset($_POST)) {
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 	<title>UB CSE Peer Evaluation</title>
-	<!--<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">-->
 </head>
 <body>
 	<main>
@@ -134,7 +132,7 @@ if ( !empty($_POST) && isset($_POST)) {
   		<form id="peerEval" method='post'>
 				<div class="text-primary"><p class="lead">Evaluating: <?php echo $name?></p></div>
 				<div class="progress">
-					<div class="progress-bar" role="progressbar" style="width: <?php echo($progress_pct);?>%;" aria-valuenow="<?php echo($_SESSION['group_member_number']);?>" aria-valuemin="0" aria-valuemax="<?php echo($num_of_group_members);?>"><?php echo($progress_pct);?>%</div>
+					<div class="progress-bar" role="progressbar" height="32px;" style="width: <?php echo($progress_pct);?>%;" aria-valuenow="<?php echo($_SESSION['group_member_number']);?>" aria-valuemin="0" aria-valuemax="<?php echo($num_of_group_members);?>"><b><?php echo($progress_pct);?>%</b></div>
 				</div>
 				<br>
 				<?php
