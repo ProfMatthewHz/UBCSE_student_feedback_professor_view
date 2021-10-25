18f6a7ae9a279ac367bd50bfa11e780ae46833cb
ProfMatthewHz <mhertz@buffalo.edu>
2021-10-24 20:03:45 -0400

    Added the normalization factor

--- instructor/surveyResults.php
+++ instructor/surveyResults.php
@@ -84,7 +84,7 @@ $emails = array();
 // Array mapping email address to normalized results
 $normalized = array();
 
-$stmt = $con->prepare('SELECT reviewer_email, students.name, SUM(rubric_scores.score) total_score
+$stmt = $con->prepare('SELECT reviewer_email, students.name, SUM(rubric_scores.score) total_score, COUNT(teammate_email) total_reviews
                        FROM reviewers INNER JOIN students ON reviewers.reviewer_email=students.email LEFT JOIN evals ON evals.reviewers_id=reviewers.id LEFT JOIN scores2 ON scores2.eval_id=evals.id LEFT JOIN rubric_scores ON rubric_scores.id=scores2.score_id WHERE survey_id=? GROUP BY reviewer_email, students.name');
 $stmt->bind_param('i', $sid);
 $stmt->execute();
@@ -96,7 +96,7 @@ while ($row = $result->fetch_array(MYSQLI_NUM)) {
   // If the reviewer completed this survey
   if (isset($row[2])) {
     // Initialize the total number of points
-    $totals[$email_addr] = $row[2];
+    $totals[$email_addr] = $row[2] / $row[3];
   }
 }
 $stmt->close();
