be5a27834c6b8b4f79da8bca914e10dd45a199cc
ProfMatthewHz <mhertz@buffalo.edu>
2022-01-17 21:24:05 -0500

    Added link so rubrics can now be created by any faculty

--- instructor/surveys.php
+++ instructor/surveys.php
@@ -139,8 +139,11 @@ foreach ($terms as $name => &$term_courses) {
         <h4 class="text-white display-1">UB CSE Evalution System<br>Instructor Overview</h4>
       </div>
     </div>
-    <div class="row justify-content-md-center mt-5 mx-4">
-      <div class="col-auto">
+    <div class="row mt-5 mx-4">
+    <div class="col">
+        <a href="addRubrics.php" class="btn btn-outline-success fs-4">+ Add Rubric</a>
+      </div>
+      <div class="col ms-auto justify-content-end">
         <a href="addCourses.php" class="btn btn-success fs-4">+ Add Class</a>
       </div>
     </div>
