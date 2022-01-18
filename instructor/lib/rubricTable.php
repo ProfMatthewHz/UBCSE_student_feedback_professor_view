c6780841dd2ae29369c82cf44a222967f180c592
Matthew Hertz <mhertz@buffalo.edu>
2022-01-18 11:00:49 -0500

    Cleaning up the handling of data

--- instructor/lib/rubricTable.php
+++ instructor/lib/rubricTable.php
@@ -1,12 +1,12 @@
 <?php
   function emitRubricTable($topics, $scores) {
-    echo '<div class="col-2"><b>Criterion</b></div>';
+    $ret_val = '<div class="col-2"><b>Criterion</b></div>';
     foreach ($scores as $score_id => $score_data) {
       $score_name = $score_data["name"];
       $score_points = $score_data["score"];
-      echo '<div class="col-2 ms-auto"><b>'.$score_name.' ('.$score_points.' pts)</b></div>';
+      $ret_val = $ret_val.'<div class="col-2 ms-auto"><b>'.$score_name.' ('.$score_points.' pts)</b></div>';
     }
-    echo '</div>';
+    $ret_val = $ret_val.'</div>';
     $shaded = true;
     foreach ($topics as $topic_id => $topic_data) {
         if ($shaded) {
@@ -16,14 +16,15 @@
         }
         $name = $topic_data["question"];
         $responses = $topic_data["responses"];
-        echo '<div class="row py-2 mx-1 align-items-stretch border-bottom border-1 border-secondary" style="background-color:'.$bg_color.'">';
-        echo '  <div class="col-2 text-center"><b>'.$name.'</b></div>';
+        $ret_val = $ret_val.'<div class="row py-2 mx-1 align-items-stretch border-bottom border-1 border-secondary" style="background-color:'.$bg_color.'">';
+        $ret_val = $ret_val.'  <div class="col-2 text-center"><b>'.$name.'</b></div>';
         foreach (array_keys($scores) as $score_id) {
           $response_text = $responses[$score_id];
-          echo '<div class="col-2 ms-auto">'.$response_text.'</div>';
+          $ret_val = $ret_val.'<div class="col-2 ms-auto">'.$response_text.'</div>';
         }
-        echo '</div>';
+        $ret_val = $ret_val.'</div>';
         $shaded = !$shaded;
     }
+    return $ret_val;
   }
 ?>
