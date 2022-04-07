82f2cc6492f5570d2ccf911b3cb4fca65fc0178f
ProfMatthewHz <mhertz@buffalo.edu>
2022-04-07 10:37:05 -0400

    Created code to enable updating the roster

--- instructor/courseAdd.php
+++ instructor/courseAdd.php
@@ -14,7 +14,7 @@ require_once "../lib/database.php";
 require_once "../lib/constants.php";
 require_once "../lib/infoClasses.php";
 require_once "../lib/fileParse.php";
-
+require_once "lib/rosterFunctions.php";
 
 //query information about the requester
 $con = connectToDatabase();
@@ -93,7 +93,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errorMsg['roster-file'] = 'An error occured when uploading the file. Please try again.';
   }
   // start parsing the file
-  else{
+  else {
     $file_handle = @fopen($_FILES['roster-file']['tmp_name'], "r");
 
     // catch errors or continue parsing the file
@@ -127,34 +127,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
             // get the inserted course id
             $course_id = $con->insert_id;
 
-
-            // now insert the roster into the roster database and the student database if needed
-            $roster_size = count($names_emails);
-
-            // prepare sql statements
-            $stmt_check = $con->prepare('SELECT student_id FROM students WHERE email=?');
-            $stmt_news = $con->prepare('INSERT INTO students (email, name) VALUES (?, ?)');
-
-            for ($i = 0; $i < $roster_size; $i ++) {
-              $stmt_check->bind_param('s', $names_emails[$i][1]);
-              $stmt_check->execute();
-              $result = $stmt_check->get_result();
-              $student_info = $result->fetch_all(MYSQLI_ASSOC);
-              $student_id = NULL;
-
-              // check if the student already exists if they don't insert them
-              if ($result->num_rows == 0)
-              {
-                $stmt_news->bind_param('ss', $names_emails[$i][1], $names_emails[$i][0]);
-                $stmt_news->execute();
-
-                $student_id = $con->insert_id;
-              }
-              else
-              {
-                $student_id = $student_info[0]['student_id'];
-              }
-            }
+            uploadRoster($con, $names_emails);
 
             // redirect to course page with message
             $_SESSION['course-add'] = "Successfully added course: " . htmlspecialchars($course_code) . ' - ' . htmlspecialchars($course_name) . ' - ' . SEMESTER_MAP_REVERSE[$semester] . ' ' . htmlspecialchars($course_year);
--- instructor/surveys.php
+++ instructor/surveys.php
@@ -128,6 +128,24 @@ foreach ($terms as $name => &$term_courses) {
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  <script>
+    function updateRoster(course_id) {
+      // Create the fake form we are uploading
+      let formData = new FormData();
+      formData.append("roster-file", document.getElementById("roster-file"+course_id).files[0]);
+      fetch('rosterUpdate.php', {method: "POST", body: formData})
+        .then(res => res.json())
+        .then(data => {
+          if (data.success) {
+            alert("Roster successfully uploaded");
+            document.getElementById("modalClose"+course_id).click();
+          } else {
+            document.getElementById("modalClose"+course_id).classList.add("is-invalid");
+            document.getElementById("roster-file-label"+course_id).innerHTML = data.error;
+          }
+        });
+    }
+  </script>
   <title>CSE Evaluation Survey System - Instuctor Overview</title>
 </head>
 <body class="text-center">
