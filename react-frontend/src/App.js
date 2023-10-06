fa043a6351617f29d30a43e62f47e035558b7a5a
Justin Variara <jrvariar@buffalo.edu>
2023-10-06 15:49:32 -0400

    Added folders for frontend and backend, moved php files to backend folder and used create-react-app in frontend folder. Added boilerplate code in App.js for the frontend

---
+++ react-frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* Add Routes here with a component to render at that Route */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
