50fa8f6163530735538679b45eaa8fd91dfe0476
Addaxe <78458403+Addaxe@users.noreply.github.com>
2023-10-14 16:54:07 -0400

    Uploading sidebar component files so it has more general use
    The sidebar component has a more general use and can adjust to any page when needed now

---
+++ sidebar-demo-files/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import SideBar from './Components/Sidebar';
+import Navbar from './Components/Navbar';
+import Home from './pages/Home';
+import History from './pages/History';
+import Library from './pages/Library';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          {/* Add Routes here with a component to render at that Route */}
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/library" element={<Library />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
