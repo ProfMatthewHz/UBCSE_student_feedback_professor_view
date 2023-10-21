22dbdccbd5ed39874f8e4708f0772c1f8d9c960b
Justin Variara <jrvariar@buffalo.edu>
2023-10-21 13:36:15 -0400

    Moved the fetch function from App.jsx into Home.jsx and History.jsx

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -4,70 +4,8 @@ import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
 import History from "./pages/History";
 import Library from "./pages/Library";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [courses, setCourses] = useState([]);
-
-  const getCurrentYear = () => {
-    const date = new Date();
-    return date.getFullYear();
-  };
-
-  // Using 2023-2024 course schedule
-  const getCurrentSemester = () => {
-    const date = new Date();
-    const month = date.getMonth(); // 0 for January, 1 for February, etc.
-    const day = date.getDate();
-
-    // Summer Sessions (May 30 to Aug 18)
-    if (
-      (month === 4 && day >= 30) ||
-      (month > 4 && month < 7) ||
-      (month === 7 && day <= 18)
-    ) {
-      return 3; // Summer
-    }
-
-    // Fall Semester (Aug 28 to Dec 20)
-    if (
-      (month === 7 && day >= 28) ||
-      (month > 7 && month < 11) ||
-      (month === 11 && day <= 20)
-    ) {
-      return 4; // Fall
-    }
-
-    // Winter Session (Dec 28 to Jan 19)
-    if ((month === 11 && day >= 28) || (month === 0 && day <= 19)) {
-      return 1; // Winter
-    }
-
-    // If none of the above conditions are met, it must be Spring (Jan 24 to May 19)
-    return 2; // Spring
-  };
-
-  useEffect(() => {
-    fetch(
-      "http://localhost/StudentSurvey/backend/instructor/instructorCoursesInTerm.php",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-          instructor_id: 1,
-          semester: getCurrentSemester(),
-          year: getCurrentYear(),
-        }),
-      }
-    )
-      .then((res) => res.json())
-      .then((result) => {
-        setCourses(result);
-        console.log(result);
-      });
-  }, []);
 
   return (
     <Router basename="/StudentSurvey/react-frontend/build">
@@ -76,8 +14,8 @@ function App() {
         <Navbar />
         <Routes>
           {/* Add Routes here with a component to render at that Route */}
-          <Route path="/" element={<Home courses={courses} />} />
-          <Route path="/history" element={<History courses={courses} />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
           <Route path="/library" element={<Library />} />
         </Routes>
       </div>
--- react-frontend/src/pages/History.jsx
+++ react-frontend/src/pages/History.jsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SideBar from "../Components/Sidebar";
 import Course from "../Components/Course";
+import "../styles/home.css";
 
-const History = ({ courses }) => {
+const History = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "http://localhost/StudentSurvey/backend/instructor/instructorCoursesInTerm.php",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          instructor_id: 1,
+          semester: 2,
+          year: 2024,
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        setCourses(result);
+        console.log(result);
+      });
+  }, []);
 
   const sidebar_content = {
-    "Terms": []
-    ,
-    "Courses": courses ? (courses.map((course) => course.code)): ([])
-  }
+    Terms: [],
+    Courses: courses ? courses.map((course) => course.code) : [],
+  };
 
   return (
     <>
-      <SideBar route="/history" content_dictionary={sidebar_content}/>
-      <div className="container">
+      <SideBar route="/history" content_dictionary={sidebar_content} />
+      <div className="container home--container">
         {courses ? (
-          courses.map((course) => <Course key={course.id} course={course} page="history" />)
+          courses.map((course) => (
+            <Course key={course.id} course={course} page="history" />
+          ))
         ) : (
-          <h1>No courses yet.</h1>
+          <div className="no-course">
+            <h1>No courses found.</h1>
+          </div>
         )}
       </div>
     </>
