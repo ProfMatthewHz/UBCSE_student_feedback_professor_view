6d925acb681d346f8a5a09fc118fcf99a8bc5e25
korliu <koreyliu1221@gmail.com>
2023-11-10 02:01:38 -0500

    backend/instructor/confirmationForSurvey.php handles POST request

--- backend/instructor/confirmationForSurvey.php
+++ backend/instructor/confirmationForSurvey.php
@@ -31,35 +31,40 @@ if (!isset($_SESSION['id'])) {
 }
 $instructor_id = $_SESSION['id'];
 
-$response = array();
-$response['data'] = array();
-$response['errors'] = array();
 
-if($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // Verify we have already defined all the data required for this conformation.
-  if (!isset($_SESSION["survey_data"]) || !isset($_SESSION["survey_course_id"]) || 
-      !isset($_SESSION["survey_file"]) || !isset($_SESSION["survey_students"])) {
+// Get the pairings we will be using in this survey
+if (!isset($_SESSION["survey_data"]) || !isset($_SESSION["survey_course_id"]) || 
+    !isset($_SESSION["survey_file"]) || !isset($_SESSION["survey_students"])) 
+{
     http_response_code(403);
     echo "Missing parameters, Survey Roster contains errors!";
     exit();
-  }
-  // Get the pairings we will be using in this survey
-  $file_data = $_SESSION["survey_file"];
-  // Get the survey data we will work with
-  $survey_data = $_SESSION["survey_data"];
-  // Get the course id of the course whose roster is being updated
-  $course_id = $_SESSION['survey_course_id'];
-  // Get information about the students in this course
-  $survey_students = $_SESSION["survey_students"];
-  // Break out the information about the survey
-  $survey_name = $survey_data['name'];
-  $survey_s = $survey_data['start'];
-  $survey_begin = $survey_s->format('M j').' at '. $survey_s->format('g:i A');
-  $survey_e = $survey_data['end'];
-  $survey_end = $survey_e->format('M j').' at '. $survey_e->format('g:i A');
-  $survey_type = $survey_data['pairing_mode'];
-  $pm_mult = $survey_data['multiplier'];
-  $rubric_id = $survey_data['rubric'];
+}
+
+
+$file_data = $_SESSION["survey_file"];
+// Get the survey data we will work with
+$survey_data = $_SESSION["survey_data"];
+// Get the course id of the course whose roster is being updated
+$course_id = $_SESSION['survey_course_id'];
+// Get information about the students in this course
+$survey_students = $_SESSION["survey_students"];
+// Break out the information about the survey
+$survey_name = $survey_data['name'];
+$survey_s = $survey_data['start'];
+$survey_begin = $survey_s->format('M j').' at '. $survey_s->format('g:i A');
+$survey_e = $survey_data['end'];
+$survey_end = $survey_e->format('M j').' at '. $survey_e->format('g:i A');
+$survey_type = $survey_data['pairing_mode'];
+$pm_mult = $survey_data['multiplier'];
+$rubric_id = $survey_data['rubric'];
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+  // Verify we have already defined all the data required for this conformation.
+
+  $response = array();
+  $response['data'] = array();
+  $response['errors'] = array();
 
   $errorMsg = array();
 
@@ -89,7 +94,8 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $errorMsg['course'] = "Forbidden. You do not teach this course.";
   }
 
-  if (!empty($errorMsg)){
+
+  if (empty($errorMsg)){
     $course_info = getSingleCourseInfo($con, $course_id, $instructor_id);
     $course_code = $course_info['code'];
     $course_name = $course_info['name'];
@@ -149,10 +155,9 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 
     $response['data'] = $rosterData;
   } else {
-
     $response['errors'] = $errorMsg;
   }
-  
+
 
   header("Content-Type: application/json; charset=UTF-8");
 
@@ -163,30 +168,44 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  if (isset($_POST["cancel-survey"]) || isset($_POST["return-survey"])) {
-    // Cancel out of the changes and exit back to the instructor home page
-    unset($_SESSION["survey_file"]);
-    unset($_SESSION["survey_data"]);
-    unset($_SESSION["survey_course_id"]);
-    unset($_SESSION["survey_students"]);
-    unset($_SESSION["pairings"]);
 
-    http_response_code(302);
-    header("Location: ".INSTRUCTOR_HOME."surveys.php");
-    exit();
-  } else {
+  $response = array('data' => array(), 'errors' => array());
+
+  if (isset($_POST["cancel-survey"]) || isset($_POST["return-survey"])) {
+    // do nothing, frontend will redirect
+
+    $response['data']['message'] = "Survey submission cancelled!";
+
+    http_response_code(200);
+
+  } elseif (isset($_POST['save-survey'])) {
     $survey_id = insertSurvey($con, $course_id, $survey_name, $survey_s, $survey_e, $rubric_id, $survey_type);
-    addReviewsToSurvey($con, $survey_id, $_SESSION['pairings']);
-    http_response_code(302);
-    header("Location: ".INSTRUCTOR_HOME."surveys.php");
-    exit();
-  }
+    
+    $addSuccess = addReviewsToSurvey($con, $survey_id, $_SESSION['pairings']);
+
+    if ($addSuccess){
+      $response['data']['message'] = "Successfully created survey and added reviews to the survey";
+    } else {
+      $response["errors"]['message'] = "Unsuccessful in adding reviews to the survey :( ";
+    }
+
+    http_response_code(200);
+  } 
+
+  unset($_SESSION["survey_file"]);
+  unset($_SESSION["survey_data"]);
+  unset($_SESSION["survey_course_id"]);
+  unset($_SESSION["survey_students"]);
+  unset($_SESSION["pairings"]);
+
+  
+  header("Content-Type: application/json; charset=UTF-8");
+
+  $responseJSON = json_encode($response);
+
+  echo $responseJSON;
+
+
+
 }
-$course_info = getSingleCourseInfo($con, $course_id, $instructor_id);
-$course_code = $course_info['code'];
-$course_name = $course_info['name'];
-$roster = getRoster($con, $course_id);
-$non_roster = getNonRosterStudents($survey_students, $roster);
-$file_results = processReviewRows($file_data, $survey_students, $pm_mult, $survey_type);
-$_SESSION["pairings"] = $file_results["pairings"];
-$roles = $file_results["roles"];
+
