f46fbd30048204de94f55b1d7ac6120d9fc72d18
korliu <koreyliu1221@gmail.com>
2023-12-06 18:12:29 -0500

    Should handle setting criterions and set session var for confirming, Test 4 in #115

--- backend/instructor/rubricSetCriterions.php
+++ backend/instructor/rubricSetCriterions.php
@@ -1,16 +1,4 @@
 <?php
-function check_level_response($crit, $level_name, $text, &$errorMsg) {
-  if (!isset($_POST[$crit."-".$level_name])) {
-    http_response_code(403);
-    echo "Forbidden: Incorrect parameters.";
-    exit();
-  }
-  $ret_val = trim($_POST[$crit."-".$level_name]);
-  if (empty($ret_val)) {
-    $errorMsg[$crit.$level_name] = "Response for ".$text." cannot be empty";
-  }
-  return $ret_val;
-}
 
 //error logging
 error_reporting(-1); // reports all errors
@@ -22,10 +10,10 @@ ini_set("error_log", "~/php-error.log");
 session_start();
 
 //bring in required code
-require_once "../../lib/database.php";
-require_once "../../lib/constants.php";
-require_once "../lib/instructorQueries.php";
-require_once "../lib/rubricQueries.php";
+require_once "../lib/database.php";
+require_once "../lib/constants.php";
+require_once "lib/instructorQueries.php";
+require_once "lib/rubricQueries.php";
 
 //query information about the requester
 $con = connectToDatabase();
@@ -38,89 +26,133 @@ if (!isset($_SESSION['id'])) {
 }
 $instructor_id = $_SESSION['id'];
 
-$errorMsg = array();
-$criteria = array();
-
 // Verify we have already defined the rubric basics
 if (!isset($_SESSION["rubric"])) {
   http_response_code(302);
-  header("Location: ".INSTRUCTOR_HOME."rubricAdd.php");
+  header("Location: ".INSTRUCTOR_HOME."rubricInitialize.php");
   exit();
 }
+
+$defined_rubric_name = $_SESSION['rubric']['name'];
+$defined_rubric_levels = $_SESSION['rubric']['levels'];
+// unsure if we should unset or not
+// unset($_SESSION['rubric']); 
+
+$errorMsg = array();
+
 // Check if we are revising a previous submission
-if (($_SERVER['REQUEST_METHOD'] != 'POST') && isset($_SESSION["confirm"])) {
-  $criteria = $_SESSION["confirm"]["topics"];
-} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // make sure minimum set of values exist
-  if (!isset($_POST['criterion1-question'])) {
+
+  $jsonPOST = file_get_contents('php://input');
+  $post_data = json_decode($jsonPOST, $flags=JSON_OBJECT_AS_ARRAY);
+
+  $_POST = $post_data;
+
+  if (!isset($_POST['topics'])) {
     http_response_code(400);
-    echo "Bad Request: Missing parmeters.";
+    echo "Bad Request: Missing parameters to define criteria";
     exit();
   }
+
   // check CSRF token
-  $csrf_token = getCSRFToken($con, $instructor_id);
-  if (!hash_equals($csrf_token, $_POST['csrf-token'])) {
-    http_response_code(403);
-    echo "Forbidden: Incorrect parameters.";
-    exit();
-  }
+//   $csrf_token = getCSRFToken($con, $instructor_id);
+//   if (!hash_equals($csrf_token, $_POST['csrf-token'])) {
+//     http_response_code(403);
+//     echo "Forbidden: Incorrect parameters.";
+//     exit();
+//   }
 
-  // Process data so that it is available in a manner that parallels how rubric queries prepares a rubric.
-  $crit_num = 1;
-  $crit_id = "criterion".$crit_num;
-  while (key_exists($crit_id.'-question', $_POST)) {
-    $crit_data = array();
-    
-    $crit_data["question"] = trim($_POST[$crit_id.'-question']);
-    if (empty($crit_data["question"])) {
-      $errorMsg[$crit_id.'-question'] = "Each criterion needs a description";
+  // Process criterion data post 
+  $topics = $_POST['topics'];
+
+  $criterion_num = -1;
+  $criteria = array();
+  foreach ($topics as $criterion){
+    $criterion_num++;
+    $criterion_id = "criterion-".$criterion_num;
+
+    $criterion_name = trim($criterion['question']);
+    $criterion_responses = $criterion['responses'];
+    $criterion_type = $criterion['type'];
+
+    $criterion_errors = array();
+    // check name
+    if (empty($criterion_name)) {
+      $criterion_errors['name'] = "Each criterion must have a description";
+    } elseif ( array_key_exists($criterion_name, $criteria) ) {
+      $criterion_errors['name'] = "Each criterion needs a UNIQUE description";
     }
-    // Translate the posted type to the values we actually use
-    if (empty($_POST[$crit_id.'-type'])) {
-      $crit_data["type"] = MC_QUESTION_TYPE;
-      // When this is a multiple choice question, record each of the different responses
-      $crit_data["responses"] = array();
-      foreach ($_SESSION["rubric"]["levels"]["names"] as $level_name => $text) {
-        $crit_data["responses"][$level_name] = check_level_response($crit_id, $level_name, $text, $errorMsg);
-      }
-    } else {
-      $crit_data["type"] = FREEFORM_QUESTION_TYPE;
+
+    if (count($criterion_responses) != count($defined_rubric_levels)){
+        http_response_code(400);
+        echo "Bad Request: Number of responses for each criterion must match the number of levels for rubric.";
+        exit();
     }
-    $criteria[] = $crit_data;
-    $crit_num = $crit_num + 1;
-    $crit_id = "criterion".$crit_num;
-  }
-  for ($i = 1; $i < $crit_num; $i++) {
-    $trait = $criteria[$i-1]["question"];
-    if (!empty($trait)) {
-      for ($j = 1; $j < $crit_num; $j++) {
-        if ( ($i != $j) && ($trait == $criteria[$j-1]["question"]) ) {
-          $errorMsg["criterion".$i.'-question'] = "Each criterion needs UNIQUE description";
+    // check criterion responses
+    foreach ($criterion_responses as $level_num => $response_text){
+        $response_text = trim($response_text);
+        
+        if (empty($response_text)){
+            $criterion_errors["level-".$level_num] = "Level description cannot be empty";
         }
-      }
     }
-  }
-  if (count($errorMsg) == 0) {
-    // Prepare the rubric for cofirmation
-    $_SESSION["confirm"] = array();
+    
+    $criterion_info = array();
+    $criterion_info['responses'] = $criterion_responses;
+    $criterion_info['type'] = $criterion_type;
 
-    // Prepare the score data for confirmation
-    $_SESSION["confirm"]["scores"] = array();
-    foreach ($_SESSION["rubric"]["levels"]["names"] as $level => $name) {
-      $_SESSION["confirm"]["scores"][$level] = array("name" => $name, "score" => $_SESSION["rubric"]["levels"]["values"][$level]);
+    $criteria[$criterion_name] = $criterion_info;
+
+    if (empty($criterion_errors)){
+
+    } else {
+        $errorMsg[$criterion_id] = $criterion_errors;
     }
 
-    // Prepare the topics & their reponses for confirmation
-    $_SESSION["confirm"]["topics"] = $criteria;
-    http_response_code(302);
-    header("Location: ".INSTRUCTOR_HOME."rubricConfirm.php");
-    exit();
   }
+  unset($criterion);
+
+  // means criterion_num was never updated and that there are no criterions, no max
+  if ($criterion_num === -1){
+    $errorMsg['criteria'] = "There must be at least 1 criterion defined";
+  }
+ 
+  $errors_response = array("errors" => array());
+
+  if (empty($errorMsg)){
+
+    $defined_rubric = array('name' => $defined_rubric_name, 'levels' => $defined_rubric_levels);
+    $defined_rubric['criteria'] = array();
+
+    $num_criteria = 0;
+    foreach ($criteria as $name => $info){
+        $criterion_id = "criterion-".$num_criteria;
+        $criterion_responses = $info['responses'];
+        $criterion_type = $info['type'];
+
+        $criterion_data = array();
+
+        $defined_rubric['criteria'][$criterion_id] = $criterion_data;
+
+        $num_criteria++;
+    }
+    unset($name, $info);
+    // $rubric_criteria = $criteria
+
+    $_SESSION['rubric-preview'] = $defined_rubric;
+  } else{
+    $errors_response['errors'] = $errorMsg;
+  }
+
+  header("Content-Type: application/json; charset=UTF-8");
+  $errorsJSON = json_encode($errors_response);
+  echo $errorsJSON;
 }
 
 // Avoid problems in the verification screen from double-submitting a rubric
 unset($_SESSION["confirm"]);
 $csrf_token = createCSRFToken($con, $instructor_id);
-$level_keys_for_js = json_encode(array_keys($_SESSION["rubric"]["levels"]["names"]));
-$level_names_for_js =  json_encode(array_values($_SESSION["rubric"]["levels"]["names"]));
+// $level_keys_for_js = json_encode(array_keys($_SESSION["rubric"]["levels"]["names"]));
+// $level_names_for_js =  json_encode(array_values($_SESSION["rubric"]["levels"]["names"]));
 ?>
