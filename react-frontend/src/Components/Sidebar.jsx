bf09bbac424e806d74526f6f7a69bbcff734054b
Marian <marianhu@buffalo.edu>
2024-02-11 18:44:22 -0500

    About Page and Side Bar change complete

--- react-frontend/src/Components/Sidebar.jsx
+++ react-frontend/src/Components/Sidebar.jsx
@@ -8,13 +8,16 @@ import AddCourse from "../pages/AddCourse";
 import AddRubric from "./AddRubric";
 
 
+
 /** Combining NavBar into Side Bar*/
-
-
-
 
 function SideBar(props) {
 
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => {
+    setClicked((prev) => !prev)
+  }
 
   const [activeButton, setActiveButton] = useState(false);
   const [dropdown_value, setDropDownValue] = useState("");
@@ -121,113 +124,42 @@ function SideBar(props) {
      
 
       <div className="title">
-      <h1>TEAMWORK</h1>
-      <h1>EVALUATION</h1>
+        <h1>TEAMWORK</h1>
+        <h1>EVALUATION</h1>
       </div>
 
       <div className="sidebar">
+      <nav>
+        
+        <ul className={`${clicked ? "open" : ""}`}>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            {/* disable history in navbar */}
+            <NavLink to="/history" className="mobile-disable">History</NavLink>
+          </li>
+          <li>
+            <NavLink to="/library">Library</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+
+        {/* Hamburger menu for phone, commented out bc of hertz request with only having Home
+            May be changed in the future so just uncomment the code below and a hamburger menu will
+            show on mobile
+        */}
+        {/* <div id="nav-mobile" onClick={handleClick}>
+          <i
+            id="nav-bar"
+            className={`fas ${clicked ? "fa-times" : "fa-bars"}`}
+          ></i>
+        </div> */}
+      </nav>
   
-        {Object.entries(props.content_dictionary).map(([title, contents]) => {
-          return props.route === "/history" ? (
-
-
-            <div
-              className="sidebar-content"
-              style={title === "Courses" ? { maxHeight: "75%" } : null}
-            >
-              {(title === "Courses" && dropdown_value !== "") ||
-              title === "Terms" ? (
-                <h1>{title}</h1>
-              ) : null}
-              <div className="sidebar-list">
-                {title === "Terms" ? (
-                  Object.keys(contents).length > 0 ? (
-                    <Dropdown
-                      value={dropdown_value}
-                      onChange={setDropDownValue}
-                      options={[
-                        { value: "", label: "Select Term" },
-                        ...Object.keys(contents).map((term) => ({
-                          value: term,
-                          label: term,
-                        })),
-                      ]}
-                    />
-                  ) : (
-                    <div className="no-content">No {title}</div>
-                  )
-                ) : title === "Courses" && dropdown_value !== "" ? (
-                  termContents.length > 0 ? (
-                    termContents.map((item) => {
-                      return (
-                        <a href={"#" + item.code}>
-                          <div
-                            onClick={() =>
-                              setActiveButton(item.code + "-Option")
-                            }
-                            id={item.code + "-Option"}
-                            className={
-                              activeButton === item.code + "-Option"
-                                ? "active"
-                                : item.code + "-Option"
-                            }
-                          >
-                            {item.code}
-                          </div>
-                        </a>
-                      );
-                    })
-                  ) : (
-                    <div className="no-content">No {title}</div>
-                  )
-                ) : null}
-              </div>
-            </div>
-          ) : (
-            <div className="sidebar-content" style={{ minHeight: "90%" }}>
-              <h1>{title}</h1>
-              <div className="sidebar-list">
-                {contents.length > 0 ? (
-                  contents.map((item) => {
-                    return (
-                      <a href={"#" + item}>
-                        <div
-                          onClick={() => setActiveButton(item + "-Option")}
-                          id={item + "-Option"}
-                          className={
-                            activeButton === item + "-Option"
-                              ? "active"
-                              : item + "-Option"
-                          }
-                        >
-                          {item}
-                        </div>
-                      </a>
-                    );
-                  })
-                ) : (
-                  <div className="no-content">No {title}</div>
-                )}
-              </div>
-              {props.route === "/" ? (
-                <button
-                  className="add_course-btn"
-                  onClick={handleAddCourseModal}
-                >
-                  + Add Course
-                </button>
-              ) : props.route === "/library" ? (
-                <button 
-                  className="add_course-btn" 
-                  onClick={handleAddRubricModal}
-                >
-                  + Add Rubric
-                </button>
-              ) : 
-              null}
-            </div>
-          );
-        })}
+       
       </div>
     </>
   );
