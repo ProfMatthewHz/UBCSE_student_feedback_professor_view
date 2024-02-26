f5e329ef04b4847db6985e42454479d808c4ae25
Matthew Prier <59553462+MPrier@users.noreply.github.com>
2024-02-25 21:13:37 -0500

    Show respective pairing mode image when more info button clicked in drop down

--- react-frontend/src/Components/NewDropDown.jsx
+++ react-frontend/src/Components/NewDropDown.jsx
@@ -3,9 +3,41 @@ import "../styles/newdropdown.css";
 
 const NewDropDown = ({pm}) => {
   const [open, setOpen] = useState(false);
+  const [showImage, setShowImage] = useState(false);
+  const [image, setImage] = useState("");
+
   const handleOpen = () => {
     setOpen(!open);
   };
+
+  const handleImageClick = (pairingMode,e) => {
+    e.stopPropagation();
+    if(open) {
+      switch(pairingMode) {
+        case 'Each Team Member Reviewed by Entire Team + Manager':
+          setImage("../src/assets/pairingmodes/team+manager+self.jpg");
+          setShowImage(!showImage);
+          break;
+        case 'Individual Reviewed by Individual':
+          setImage("../src/assets/pairingmodes/single-pairs.jpg");
+          setShowImage(!showImage);
+          break;
+        case 'Each Team Member Reviewed By Entire Team':
+          setImage("../src/assets/pairingmodes/team.jpg");
+          setShowImage(!showImage);
+          break;
+        case 'Single Individual Reviewed by Each Team Member':
+          setImage("../src/assets/pairingmodes/team+self.jpg");
+          setShowImage(!showImage);
+          break;
+        default:
+          console.log('Unexpected pairing mode: ${pairingMode}');
+          break;
+      }
+    }
+
+  
+  }
   return (
     <div className="dropdown">
       <button className="dropdown-button" onClick={handleOpen}>
@@ -23,10 +55,15 @@ const NewDropDown = ({pm}) => {
         {pm.map((pairing) => (
           <li className="menu-item" key={pairing} value={pairing}>
             <button className="pair">{pairing}</button>
-            <button className="pairing-mode-visual">Test</button>
+            <button className="pairing-mode-visual" onClick={(e) => handleImageClick(pairing, e)}>Test</button>
           </li>
         ))}
       </ul>) : null}
+      {showImage ? (
+        <div className="pairing-mode-image">
+          <img src={image} alt="Pairing Mode Visual"/>
+        </div>
+      ) : null}
     </div>
   );
 };
