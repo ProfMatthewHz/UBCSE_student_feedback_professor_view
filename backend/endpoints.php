cd84ea67dc7588a7304abc195403c7b70a42627a
tariq <mnazeem@buffalo.edu>
2024-03-04 22:35:15 -0500

    endpoint created to fetch upcoming, existing or past surveys

--- backend/endpoints.php
+++ backend/endpoints.php
@@ -9,13 +9,19 @@ require "lib/studentQueries.php";
 require "lib/database.php";
 require "lib/surveyQueries.php";
 
+header('Content-Type: application/json');
+
 $con = connectToDatabase();
 $month = idate('m');
 $term = MONTH_MAP_SEMESTER[$month];
 $year = idate('Y');
 
-if(!empty($_SERVER['student_id'])) {
-    header("Location: ".SITE_HOME);
+$response = [];
+
+if(empty($_SERVER['student_id'])) {
+    http_response_code(400);
+    $response['error'] = 'missing student_id';
+    echo json_encode($response);
     exit();
 }
 
@@ -24,6 +30,87 @@ $student_id = $_SERVER['student_id'];
 $past_surveys = getClosedSurveysForTerm($con, $term, $year, $student_id);
 $current_surveys = getCurrentSurveys($con,   $student_id);
 $upcoming_surveys = getUpcomingSurveys($con,   $student_id);
+
+// grabbing all the past surveys //
+$pastSurveysResponse = [];
+
+if(count($past_surveys) > 0) {
+    foreach ($past_surveys as $key => $value) {
+        $pastSurveyData = [
+            'surveyID' => $key,
+            'courseName' => $value[0],
+            'surveyName' => $value[1],
+            'closingDate' => $value[2]
+        ];
+        $pastSurveysResponse[] = $pastSurveyData;
+    }
+} else {
+    $pastSurveysResponse[] = 'No closed surveys for this term';
+}
+
+// grab all the current surveys //
+$currentSurveysResponse = [];
+
+if(count($current_surveys) > 0) {
+    foreach ($current_surveys as $key => $value) {
+        $currentSurveyData = [
+            'surveyID' => $key,
+            'courseName' => $value[0],
+            'surveyName' => $value[1],
+            'closingDate' => $value[2]
+        ];
+        $currentSurveysResponse[] = $currentSurveyData;
+    }
+} else {
+    $currentSurveysResponse[] = 'No active surveys for this term';
+}
+
+// grab all the upcoming surveys //
+$upcomingSurveysResponse = [];
+
+if(count($upcoming_surveys) > 0) {
+    foreach ($upcoming_surveys as $key => $value) {
+        $upcomingSurveyData = [
+            'surveyID' => $key,
+            'courseName' => $value[0],
+            'surveyName' => $value[1],
+            'closingDate' => $value[2]
+        ];
+        $upcomingSurveysResponse[] = $upcomingSurveyData;
+    }
+} else {
+    $currentSurveysResponse[] = 'Nothing planned yet. Check back later!';
+}
+
+
+// get users surveys api endpoint //
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['type'])) {
+        $type = $_GET['type'];
+        switch ($type) {
+            case 'upcoming':
+                $response = $upcomingSurveysResponse;
+                break;
+            case 'current':
+                $response = $currentSurveysResponse;
+                break;
+            case 'past':
+                $response = $pastSurveysResponse;
+                break;
+            default:
+                http_response_code(400); // Bad request
+                $response = ['error' => 'Invalid survey type specified'];
+                break;
+
+        }
+    } else {
+        http_response_code(400); // Bad request
+        $response = ['error' => 'No survey type specified'];
+    }
+
+    echo json_encode($response);
+}
+
 
 
 
