7b2282a62562cda8ded66fe368af95705831545e
Marian <marianhu@buffalo.edu>
2024-03-31 18:50:53 -0400

    view feedback count successfully shows on frontend

--- react-frontend/src/Components/AboutSidebar.jsx
+++ react-frontend/src/Components/AboutSidebar.jsx
@@ -32,6 +32,9 @@ const AboutSidebar = () => {
           <li>
             <NavLink to="/about">About</NavLink>
           </li>
+          <li>
+                <NavLink to="/student">Student Side</NavLink>  
+              </li>
         </ul>
 
         {/* Hamburger menu for phone */}
