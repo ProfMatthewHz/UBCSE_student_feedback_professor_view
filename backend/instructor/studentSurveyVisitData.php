a30467dc5f882e1c45fe103eeb1940b7cb39630d
ardianmuriqi07 <63529426+ardianmuriqi07@users.noreply.github.com>
2024-04-04 00:55:41 -0400

    upadated studentSurveyVisitData.php to handle csrf tokens functionality, and got ready for deployment.

--- backend/instructor/studentSurveyVisitData.php
+++ backend/instructor/studentSurveyVisitData.php
@@ -22,12 +22,18 @@ if (!isset($_SESSION['id'])) {
 
 // Validate CSRF token early in the script
 
-// Validate CSRF token early in the script
+// Validate CSRF token early in the script, this is for deployement 
 if (!isset($_SESSION['csrf_token'])) {
+    print ($csrf_token);
     http_response_code(403);
     echo json_encode(["error" => "CSRF token validation failed."]);
     exit();
 }
+
+// print($_SESSION['csrf_token']);
+// echo "\r\n";
+
+
 
 $email = isset($_GET['email']) ? urldecode($_GET['email']) : null;
 $survey_id = isset($_GET['survey_id']) ? (int) $_GET['survey_id'] : null;
