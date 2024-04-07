ea43a2e1524658d69e607174bc8d618dc89ad93f
Matthew Prier <mrprier@hotmail.com>
2024-04-06 20:28:22 -0400

    add dynamic sizing to survey

--- react-frontend/src/Components/SurveyFormRow.jsx
+++ react-frontend/src/Components/SurveyFormRow.jsx
@@ -4,6 +4,8 @@ import "../styles/surveyForm.css";
 const SurveyFormRow = ({x, surveyResults, setSurveyResults, student}) => {
     const [results, setResults] = useState([]);
     const [answered, setAnswered] = useState(0);
+    const [topicQuestionElements, setTopicQuestionElements] = useState([]);
+    const [topicQuestionWidth, setTopicQuestionWidth] = useState(150);
     
     useEffect(() => {
         if (surveyResults != null && setSurveyResults != null && answered === x.topics.length) {
@@ -12,25 +14,42 @@ const SurveyFormRow = ({x, surveyResults, setSurveyResults, student}) => {
         }
     }, [answered]);
 
+    useEffect(() => {
+        // Apply width of 250px to each element
+        topicQuestionElements.forEach(element => {
+          element.style.width = '250px';
+        });
+      }, [topicQuestionElements]);
+
     const click = (response) => {
         setAnswered(answered+1);
-        setResults([...results,{response: {response}}]);
+        setResults([...results,{response}]);
 
     }
     const topics = x.topics.map(topic => {
-        let count = -1;  
+        let count = -1;
+        let width = 150;  
+        const length = Object.keys(topic.responses).length;
+        if (topic.question.length > 60 && topicQuestionWidth != 250 ) {
+            const elements = document.getElementsByClassName('row-topic-question-container');
+            setTopicQuestionElements(Array.from(elements));
+            setTopicQuestionWidth(250);
+
+        } 
         return (
             <div className='row-container'>
                 {/* <div className='vertical-line'></div>
                 <h3 className='row-topic-question'>{topic.question}</h3>     */}
                     <div className='vertical-line'>
-                        <div className='row-topic-question-container'>
-                            {topic.question}
+                        <div className='row-topic-question-container' style={{'min-width': topicQuestionWidth +'px'}}>
+                            
+                            <span className='question' >{topic.question}</span>
                         </div>
+                        {console.log(Object.keys(topic.responses).length)}
                             {Object.values(topic.responses).map((response, index) => {
                                 return (
-                                    <div className='table-data-container'>
-                                        <button onClick={() => click(response) } className='response-button'>{response}</button>
+                                    <div className='table-data-container' style={{width: 100 / length +'%'}}>
+                                        <button onClick={() => click(response) } className='response-button' style={{'font-size': 100 - (response.length / 4) +'%'}}>{response}</button>
                                     </div>    
                                 )})}
                     </div>    
