cf10f45f71a822ff891e0e6794c64cab041bd7be
tariq <mnazeem@buffalo.edu>
2024-04-08 00:39:30 -0400

    commmented out csrf token stuff

--- backend/endpoints.php
+++ backend/endpoints.php
@@ -19,13 +19,13 @@ if(!isset($_SESSION['student_id'])) {
     exit();
 }
 
-// Validate CSRF token early in the script, this is for deployement
-if (!isset($_SESSION['csrf_token'])) {
-    http_response_code(403);
-    echo json_encode(["error" => "CSRF token validation failed."]);
-    exit();
-}
-print($_SESSION['csrf_token']);
+//// Validate CSRF token early in the script, this is for deployement
+//if (!isset($_SESSION['csrf_token'])) {
+//    http_response_code(403);
+//    echo json_encode(["error" => "CSRF token validation failed."]);
+//    exit();
+//}
+//print($_SESSION['csrf_token']);
 
 header('Content-Type: application/json');
 
