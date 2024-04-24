cbaba79d30196ff833d987a0495bad7d7928c703
tariq <mnazeem@buffalo.edu>
2024-04-23 21:42:07 -0400

    Adapted to allow frontend to send scoreText; deduces score_id from score text.

--- backend/buildPeerEvalForm.php
+++ backend/buildPeerEvalForm.php
@@ -20,11 +20,16 @@ if(!isset($_SESSION['student_id'])) {
     exit();
 }
 
+if(!isset($_SESSION['mc_answers'])) {
+    header("Location: ". "https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-302a/StudentSurvey/backend/unified_fake_Shibboleth.php");
+    exit();
+}
+
 /* expected response :
 review_id -> x,
 
     responses -> [
-    TopicId 1-> score,
+    TopicId 1-> score is going to be text, have a lookup table that correctly matches text with score,
     TopicId 2-> score,
     TopicId 3-> score,
     TopicId 4-> score,
@@ -36,10 +41,9 @@ review_id -> x,
 $postData = json_decode(file_get_contents('php://input'), true);
 
 if (!empty($postData)) {
-    echo 'Recieved Data';
+
     if (!isset($postData['review_id'], $postData['responses'])) {
         http_response_code(400);
-        echo 'bad';
         $responseArray[] = "Bad Request: Missing POST parameters";
         exit();
     }
@@ -47,9 +51,6 @@ if (!empty($postData)) {
     $review_id = $postData['review_id'];
     $review_id = filter_var($review_id, FILTER_SANITIZE_NUMBER_INT);
     $eval_id = getEvalForReview($con, $review_id);
-    echo 'eval_id: ';
-    echo $eval_id;
-    
 
     if (empty($eval_id)) {
         $student_scores=array();
@@ -59,14 +60,33 @@ if (!empty($postData)) {
         $student_scores=getEvalScores($con, $eval_id);
     }
 
+    $mc_answers = $_SESSION['mc_answers'];
+
     // response array //
     $response = $postData['responses'];
     // for each response, update or add score //
     foreach ($response as $topic_id => $score) {
-        $score_data = json_decode($score, true);
+
         $topic_id = filter_var($topic_id, FILTER_SANITIZE_NUMBER_INT);
-        $score_id = filter_var($score, FILTER_SANITIZE_NUMBER_INT);
-        echo $score_id;
+        $score_id = -1;
+        // find score id //
+        //  "mc answers":{"1":{"1":"Does not willingly assume team roles, rarely completes assigned work","2":"Usually accepts assigned
+        //  team roles, occasionally completes assigned work","3":"Accepts assigned team roles, mostly completes assigned
+        //  work","4":"Accepts all assigned team roles, always completes assigned work"},..
+        $searchArr = $mc_answers[$topic_id];
+        foreach ($searchArr as $current_id => $textVal) {
+            if ($textVal == $score) {
+                $score_id = $current_id;
+                break;
+            }
+        }
+
+        if ($score_id == -1) {
+            http_response_code(400);
+            $responseArray[] = "Bad Request: score text doesnt correspond to a valid score_id";
+            exit();
+        }
+
         if (array_key_exists($topic_id, $student_scores)) {
             // Update the existing score if it exists
             updateExistingScore($con, $eval_id, $topic_id, $score_id);
@@ -74,11 +94,10 @@ if (!empty($postData)) {
             // Insert a new score if it had not existed
             insertNewScore($con, $eval_id, $topic_id, $score_id);
         }
+
     }
 
 
-} else {
-    echo 'empty';
 }
 
 
