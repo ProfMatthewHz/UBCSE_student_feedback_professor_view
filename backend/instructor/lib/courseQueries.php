9efec1b22693d9099e3b9650ce786162a8d9d24b
Matthew Hertz <mhertz@buffalo.edu>
2024-07-05 13:10:55 -0400

    Continuing to clean up this code

--- backend/instructor/lib/courseQueries.php
+++ backend/instructor/lib/courseQueries.php
@@ -50,75 +50,6 @@ function isCourseInstructor($con, $course_id, $instructor_id) {
 }
 
 
-function getSurveysForCourses($con, &$terms) {
-  $today = new DateTime();
-
-  // Now get data on all of the surveys in each of those courses
-  $stmt = $con->prepare('SELECT name, start_date, end_date, rubric_id, surveys.id, COUNT(reviews.id) AS total, COUNT(evals.id) AS completed
-                         FROM surveys
-                         LEFT JOIN reviews ON reviews.survey_id=surveys.id
-                         LEFT JOIN evals ON evals.review_id=reviews.id
-                         WHERE course_id=?
-                         GROUP BY name, start_date, end_date, rubric_id
-                         ORDER BY start_date DESC, end_date DESC');
-  foreach ($terms as $name => &$term_courses) {
-    foreach($term_courses as $id => &$course) {
-      // Get the course's surveys in reverse chronological order
-      $stmt->bind_param('i', $id);
-      $stmt->execute(); 
-      $result = $stmt->get_result();
-      while ($row = $result->fetch_assoc()) {
-        $survey_info = array();
-        $survey_info['course_id'] = $id;
-        $survey_info['name'] = $row['name'];
-        $survey_info['start_date'] = $row['start_date'];
-        $survey_info['end_date'] = $row['end_date'];
-        $survey_info['rubric_id'] = $row['rubric_id'];
-        $survey_info['id'] = $row['id'];
-        // Generate and store that progress as text
-        $percentage = 0;
-        if ($row['total'] != 0) {
-          $percentage = floor(($row['completed'] / $row['total']) * 100);
-        }
-        $survey_info['completion'] = $percentage . '% completed';
-
-        // determine status of survey. then adjust dates to more friendly format
-        $s = new DateTime($survey_info['start_date']);
-        $e = new DateTime($survey_info['end_date']);
-        $survey_info['sort_start_date'] = $survey_info['start_date'];
-        $survey_info['sort_expiration_date'] = $survey_info['end_date'];
-        $survey_info['start_date'] = $s->format('M j').' at '. $s->format('g:i A');
-        $survey_info['end_date'] = $e->format('M j').' at '. $e->format('g:i A');
-
-        if ($today < $s) {
-          $course['upcoming'][] = $survey_info;
-        } else if ($today < $e) {
-          $course['active'][] = $survey_info;
-        } else {
-          $course['expired'][] = $survey_info;
-        }
-      }
-    }
-    unset($course);
-  }
-  $stmt->close();
-  return $terms;
-}
-
-function getAllCoursesForInstructor($con, $instructor_id) {
-  $stmt = $con->prepare('SELECT id, code, name, semester, year 
-                         FROM courses
-                         INNER JOIN course_instructors ON courses.id=course_instructors.course_id
-                         WHERE instructor_id=? 
-                         ORDER BY year DESC, semester DESC, code DESC');
-  $stmt->bind_param('i', $instructor_id);
-  $stmt->execute();
-  $result = $stmt->get_result();
-  $course_info = $result->fetch_all(MYSQLI_ASSOC);
-  $stmt->close();
-  return $course_info;
-}
-
 function getSingleCourseInfo($con, $course_id, $instructor_id) {
   // Pessmisticaly assume that the course fails
   $retVal = null;
@@ -137,7 +68,6 @@ function getSingleCourseInfo($con, $course_id, $instructor_id) {
   return $retVal;
 }
 
-//Korey wrote this 
 function getInstructorTermCourses($con, $instructor_id, $semester, $year){
   $retVal = array();
 
@@ -156,7 +86,7 @@ function getInstructorTermCourses($con, $instructor_id, $semester, $year){
   $stmt->close();
   return $retVal;
 } 
-// korey wrote this 
+
 function getSurveysFromSingleCourse($con, $course_id){
 
   $retVal = array();
